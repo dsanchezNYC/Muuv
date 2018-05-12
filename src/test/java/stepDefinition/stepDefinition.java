2bf320f25c3933a7bc8e289c8eb6201c3df284c5
DSanchez <daniel.edw.sanchez@gmail.com>
2018-05-12 16:27:09 -0400

    Two new scenarios (Log In page, Sign Up modal)

--- src/test/java/stepDefinition/stepDefinition.java
+++ src/test/java/stepDefinition/stepDefinition.java
@@ -1,6 +1,7 @@
 package stepDefinition;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -27,16 +28,43 @@ public class stepDefinition {
         WebDriverWait waitLogin= new WebDriverWait(driver, 10);
         //waitLogin.until(ExpectedConditions.elementToBeClickable(By.linkText("Sign Up")));
         waitLogin.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='signup-root']")));
-        //throw new PendingException();
     }
 
     @Then("^I should see landing page$")
     public void i_should_see_landing_page() throws Throwable {
         driver.findElement(By.xpath("//*[@class='signup-root']"));
-        //throw new PendingException();
-
+        driver.findElement(By.xpath("//*[@class='login-root splash-login']"));
         driver.close();
         driver.quit();
     }
 
+    @When("^I click Sign Up$")
+    public void i_click_Sign_Up() throws Throwable {
+        WebDriverWait waitLogin= new WebDriverWait(driver, 10);
+        waitLogin.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='signup-root']")));
+        driver.findElement(By.xpath("//*[@class='signup-root']")).click();
+    }
+
+    @When("^I click Log In$")
+    public void i_click_Log_In() throws Throwable {
+        WebDriverWait waitLogin= new WebDriverWait(driver, 10);
+        waitLogin.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='login-root splash-login']")));
+        driver.findElement(By.xpath("//*[@class='login-root splash-login']")).click();
+    }
+
+    @Then("^I can view sign up modal")
+    public void iCanViewSignUpModal() throws Throwable {
+        WebDriverWait waitLogin= new WebDriverWait(driver, 10);
+        waitLogin.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='signup-submit-button']")));
+        driver.close();
+        driver.quit();
+    }
+
+    @Then("^I can view log in page$")
+    public void iCanViewLogInPage() throws Throwable {
+        WebDriverWait waitLogin= new WebDriverWait(driver, 10);
+        waitLogin.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='login-submit-button']")));
+        driver.close();
+        driver.quit();
+    }
 }
--- src/test/resources/LandingPage.feature
+++ src/test/resources/LandingPage.feature
@@ -4,4 +4,12 @@ Feature: Muuv Landing Page
     Given I open Muuv
     Then I should see landing page
 
+  Scenario: Sign Up
+    Given I open Muuv
+    When I click Sign Up
+    Then I can view sign up modal
 
+  Scenario: Log In
+    Given I open Muuv
+    When I click Log In
+    Then I can view log in page
